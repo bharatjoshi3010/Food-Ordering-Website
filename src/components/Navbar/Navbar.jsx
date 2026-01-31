b828f9c76e892f2b9004ab59d2eeed3b2b66f0ae
unknown <bj20032003@gmail.com>
2026-02-01 02:13:34 +0530

    Make the website responsive through media-query and added the #id ref to the navbar menu buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -11,10 +12,11 @@ const Navbar = () => {
       <img className="logo" src={assets.logo} alt="" />
       <ul className="navbar-menu">
         {/* the usestate variable - menu will change when we click on these so it will show a Underline(we set it in 'active' class) on the option which is setted on menu that time  */}
-        <li onClick={()=>{setMenu("home")}} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>{setMenu("menu")}} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=>{setMenu("mobile-app")}} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
-        <li onClick={()=>{setMenu("contact-us")}} className={menu==="contact-us"?"active":""}>Contact Us</li>
+        <Link to='/' onClick={()=>{setMenu("home")}} className={menu==="home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>{setMenu("menu")}} className={menu==="menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>{setMenu("mobile-app")}} className={menu==="mobile-app"?"active":""}>Mobile-app</a>
+        <a href='#footer' onClick={()=>{setMenu("contact-us")}} className={menu==="contact-us"?"active":""}>Contact Us</a>
+        {/* these href take you to the components having these 'id' */}
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
