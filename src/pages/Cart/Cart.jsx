c622547139f97e11c986baa0838f70fa9fcbb4cc
unknown <bj20032003@gmail.com>
2026-02-01 18:58:02 +0530

    placeorder page is now working fine

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
   //bringing the CartItems from the contextApi
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext)
+
+  const navigate = useNavigate();   //used to navigate from this page to another page
 
   return (
     <div className='cart'>
@@ -44,20 +47,20 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>{getTotalCartAmount()}</p>
+              <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>Procced To Checkout</button>
+          <button onClick={()=>navigate('/order')}>Procced To Checkout</button>
         </div>
         <div className='cart-promocode'> 
           <div>
