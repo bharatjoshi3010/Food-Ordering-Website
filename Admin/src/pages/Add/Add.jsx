46b208f1d7ad4590808059fdf70f63b5e4357fa9
unknown <bj20032003@gmail.com>
2026-02-04 11:21:15 +0530

    Form change updates handled for ADD page of admin panel

--- Admin/src/pages/Add/Add.jsx
+++ Admin/src/pages/Add/Add.jsx
@@ -1,50 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
 
 const Add = () => {
-  return (
-    <div className='add'>
-      <form action="" className='flex-col'>
-        <div className='add-img-upload flex-col'>
-            <p>Upload Image</p>
-            <label htmlFor="image">
-                <img src={assets.upload_area} alt="" />
-            </label>
-            <input type="file" id="image" hidden required/>
-            {/* this htmlFor attribute of <label> is connecting this label with <input> having id attribute same as htmlFor */}
+
+    // making state variable for storing the image
+    const [image, setImage] = useState(false);
+    //making a state object variable for storing all other fields
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        // extracted name and value from this event
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    // This function is a universal onChange handler for form inputs: when any input field 
+    // changes,it receives the event object, where `event.target` refers to the input element that triggered
+    // the change. From this target, `event.target.name` gives the field identifier and
+    // `event.target.value` gives the current user-entered value.
+    // Example: for <input name="email" value="user@gmail.com" onChange={onChangeHandler} />,
+    // event.target.name === "email" and event.target.value === "user@gmail.com".
+    // The function then updates React state using setData by copying the previous state and
+    // dynamically updating only the matching key ([name]: value), so other form fields remain
+    // unchanged and the state update stays safe even with asynchronous renders.
+
+
+    //to chechk this is working properly or not
+    // useEffect(()=>{
+    //     console.log(data);
+    // },[data])
+
+    
+
+    return (
+        <div className='add'>
+            <form action="" className='flex-col' onSubmit={onSubmitHandler}>
+                <div className='add-img-upload flex-col'>
+                    <p>Upload Image</p>
+                    <label htmlFor="image">
+                        <img src={image ? URL.createObjectURL(image) : assets.upload_area} alt="" />
+                        {/* this will show a upload logo if any image is not set, and if we select a image then -> 
+                URL.createObjectURL(image) -> it will convert that object to url and in this block that selected image will be displayed */}
+                    </label>
+                    <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden required />
+                    {/* this htmlFor attribute of <label> is connecting this label with <input> having id attri
+            bute same as htmlFor */}
+                    {/* onchange :-> it will trigger when user select an image and store the first selected image in 'Image' state variable */}
+                </div>
+                <div className="add-product-name flex-col">
+                    <p>Product Name</p>
+                    <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here' />
+                </div>
+                <div className="add-product-description flex-col">
+                    <p>Product description</p>
+                    <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='Write content here' required></textarea>
+                </div>
+                <div className="add-category-price">
+                    <div className="add-category flex-col">
+                        <p>Product category</p>
+                        <select onChange={onChangeHandler} value={data.category} name="category">
+                            <option value="Salad">Salad</option>
+                            <option value="Rolls">Rolls</option>
+                            <option value="Deserts">Deserts</option>
+                            <option value="Sandwich">Sandwich</option>
+                            <option value="Cake">Cake</option>
+                            <option value="Pure Veg">Pure Veg</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Noodles">Noodles</option>
+                        </select>
+                    </div>
+                    <div className="add-price flex-col">
+                        <p>Product Price</p>
+                        <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='$20' />
+                    </div>
+                </div>
+                <button type='submit' className='add-btn'>ADD</button>
+            </form>
         </div>
-        <div className="add-product-name flex-col">
-            <p>Product Name</p>
-            <input type="text" name='name' placeholder='Type here' />
-        </div>
-        <div className="add-product-description flex-col">
-            <p>Product description</p>
-            <textarea name="description" rows="6" placeholder='Write content here' required></textarea>
-        </div>
-        <div className="add-category-price">
-            <div className="add-category flex-col">
-                <p>Product category</p>
-                <select name="category">
-                    <option value="Salad">Salad</option>
-                    <option value="Rolls">Rolls</option>
-                    <option value="Deserts">Deserts</option>
-                    <option value="Sandwich">Sandwich</option>
-                    <option value="Cake">Cake</option>
-                    <option value="Pure Veg">Pure Veg</option>
-                    <option value="Pasta">Pasta</option>
-                    <option value="Noodles">Noodles</option>
-                </select>
-            </div>
-            <div className="add-price flex-col">
-                <p>Product Price</p>
-                <input type="Number" name='price' placeholder='$20' />
-            </div>
-        </div>
-        <button type='submit' className='add-btn'>ADD</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default Add
