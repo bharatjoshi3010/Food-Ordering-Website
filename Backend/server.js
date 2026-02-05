7eca74622b59e76feb3afd8a8000c9a760e6ff56
unknown <bj20032003@gmail.com>
2026-02-05 23:39:47 +0530

    Created logic(api) for the payment with Stripe

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'     //it include env file in our project
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 //app config
@@ -28,6 +29,7 @@ app.use("/images", express.static('uploads'))  //by this we are accessing upload
 // (if you write 'http://localhost:4000/images/17700344050471762882081133.jpg' -> http://localhost:4000/images/nameOfimgStoredInUploadsFolder.jpg it in the browser it will show an image )
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res)=>{
     res.send("API working")
