34de2bd92e4cd39a523da28becc67251590d7776
unknown <bj20032003@gmail.com>
2026-02-06 22:26:43 +0530

    Admin panels food item UI is completed

--- Admin/src/pages/Orders/Orders.jsx
+++ Admin/src/pages/Orders/Orders.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import axios from "axios";
 import {toast} from "react-toastify"
 import { useEffect } from 'react';
+import { assets } from '../../assets/assets.js'
 
 const Orders = ({url}) => {
 
@@ -25,8 +26,45 @@ const Orders = ({url}) => {
   },[])
 
   return (
-    <div>
-      hi
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {
+          orders.map((order, index)=>(
+            <div key={index} className='order-item'>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                <p className='order-item-food'>
+                  {order.items.map((item, index)=>{
+                    if(index === order.items.length-1){
+                      return item.name + " X " + item.quantity
+                    }else{
+                      return item.name + " X " + item.quantity + ", "
+                    }
+                  })}
+                </p>
+                <p className="order-item-name">
+                  {
+                    order.address.firstName+" "+order.address.lastName
+                  }
+                </p>
+                <div className="order-item-address">
+                  <p>{order.address.street+", "}</p>
+                  <p>{order.address.city+", "+order.address.state+", "+ order.address.zipcode}</p>
+                </div>
+                <p className="order-item-phone">{order.address.phone}</p>
+              </div>
+              <p>Items: {order.items.length}</p>
+              <p>${order.amount}</p>
+              <select>
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out for delivery">Out for delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
