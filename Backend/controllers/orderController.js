cd396066654aec882ba8fec0bf44bf8f873fb582
Bharat Joshi <66898718+bharatjoshi3010@users.noreply.github.com>
2026-02-07 14:59:20 +0530

    Update frontend URL for placing orders

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing users order from frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-ordering-frontend-4lbx.onrender.com/"
 
     try{
         const newOrder = new orderModel({
