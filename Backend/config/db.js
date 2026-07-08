9fdfe523683a1e61941a8eebca818b195712bf85
Bharat Joshi <66898718+bharatjoshi3010@users.noreply.github.com>
2026-07-08 14:28:13 +0530

    Update database connection string in db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://joshibharat3010_db_user:helloo@cluster0.glx4t9m.mongodb.net/?appName=Cluster0').then(()=>{
-    // await mongoose.connect('mongodb+srv://manjuskitchenn:hello@cluster0.jwqf2e8.mongodb.net/food-del').then(()=>{
+    // await mongoose.connect('mongodb+srv://joshibharat3010_db_user:helloo@cluster0.glx4t9m.mongodb.net/?appName=Cluster0').then(()=>{
+    await mongoose.connect('mongodb+srv://manjuskitchenn:hello@cluster0.jwqf2e8.mongodb.net/food-del').then(()=>{
         console.log("DB Connencted");
     });
 }
